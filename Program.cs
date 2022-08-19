1c58edc1cbd3b81e8b1f9165573a1bdb22eafd75
johnbrechbuhl85 <jbrechbuhl@gmail.com>
2022-08-19 18:11:47 -0500

    Small modification to prevent the Borderless1942 program from closing when changing maps during multiplayer.
    Tested the changes using the master.bf1942.org master server patch from the link below.
    https://www.moddb.com/games/battlefield-1942/downloads/bf1942-masterbf1942org-patch

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@ using System.Diagnostics;
 // Start Process
 var processStartInfo = new ProcessStartInfo
 {
-	FileName = @"BF1942.exe"
+    FileName = @"BF1942.exe"
 };
+
 foreach (var arg in args)
 {
-	processStartInfo.ArgumentList.Add(arg);
+    processStartInfo.ArgumentList.Add(arg);
 }
+
 var process = Process.Start(processStartInfo)!;
+
 var keepAlive = true;
-Console.WriteLine($"Process has started ({process.Id})");
+Console.WriteLine($"[Borderless1942]: [{DateTime.Now:MM/dd/yyyy - hh:mm:ss tt}] [BF1942 Process Has Started]: ({process.Id})");
 
 // Wait for initial window handle
 var window = await process.WaitForMainWindowAsync();
@@ -22,37 +25,47 @@ window.RemoveBorders();
 while (keepAlive)
 {
 MainLoop:
-	UpdateWindowPosition(window);
-	await Task.Delay(100);
 
-	if (process.HasExited)
-	{
-		var oldProcessId = process.Id;
-		var retryCount = 0;
-		while (retryCount < 2)
-		{
-			var matchingProcesses = Process.GetProcessesByName("BF1942");
-			if (matchingProcesses.Length == 1)
-			{
-				process = matchingProcesses[0];
-				Console.WriteLine($"Process has changed ({oldProcessId} -> {process.Id})");
-				window = await process.WaitForMainWindowAsync();
-				window.RemoveBorders();
-				goto MainLoop;
-			}
-			retryCount++;
-			await Task.Delay(TimeSpan.FromSeconds(1));
-		}
-		keepAlive = false;
-		Console.WriteLine($"Process has exited ({oldProcessId})");
-	}
+    UpdateWindowPosition(window);
+    await Task.Delay(100);
+
+    if (process.HasExited)
+    {
+        var oldProcessId = process.Id;
+        var retryCount = 0;
+
+        while (retryCount < 2)
+        {
+            var matchingProcesses = Process.GetProcessesByName("BF1942");
+            if (matchingProcesses.Length == 1)
+            {
+                process = matchingProcesses[0];
+
+                Console.WriteLine($"[Borderless1942]: [{DateTime.Now:MM/dd/yyyy - hh:mm:ss tt}] [BF1942 Process Has Changed] [Map Change Detected] ({oldProcessId} -> {process.Id})");
+
+                if (oldProcessId == process.Id)
+                {
+                    Console.WriteLine($"[Borderless1942]: [{DateTime.Now:MM/dd/yyyy - hh:mm:ss tt}] [BF1942 Process Has Been Closed] ({oldProcessId} -> {process.Id})");
+                    keepAlive = false;
+                    break;
+                }
+
+                window = await process.WaitForMainWindowAsync();
+                window.RemoveBorders();
+
+                goto MainLoop;
+            }
+            retryCount++;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
 }
 
 static void UpdateWindowPosition(Window window)
 {
-	var monitorBounds = window.GetCurrentMonitor().GetBounds();
-	var windowBounds = window.GetBounds();
-	var x = monitorBounds.Width / 2 - windowBounds.Width / 2;
-	var y = monitorBounds.Height / 2 - windowBounds.Height / 2;
-	window.SetPosition(x, y);
+    var monitorBounds = window.GetCurrentMonitor().GetBounds();
+    var windowBounds = window.GetBounds();
+    var x = monitorBounds.Width / 2 - windowBounds.Width / 2;
+    var y = monitorBounds.Height / 2 - windowBounds.Height / 2;
+    window.SetPosition(x, y);
 }
