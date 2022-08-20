b9ac19053e9623acb0f5a3e2e93ce82b2216061b
johnbrechbuhl85 <jbrechbuhl@gmail.com>
2022-08-20 10:38:13 -0500

    Reverted white space changes and updated console messages to use ISO 8601 date format.
    Modified the "if (oldProcessId == process.Id)" condition to call Environment.Exit(0) and close Borderless1942 when BF1942 is closed.

--- Program.cs
+++ Program.cs
@@ -4,18 +4,15 @@ using System.Diagnostics;
 // Start Process
 var processStartInfo = new ProcessStartInfo
 {
-    FileName = @"BF1942.exe"
+	FileName = @"BF1942.exe"
 };
-
 foreach (var arg in args)
 {
-    processStartInfo.ArgumentList.Add(arg);
+	processStartInfo.ArgumentList.Add(arg);
 }
-
 var process = Process.Start(processStartInfo)!;
-
 var keepAlive = true;
-Console.WriteLine($"[Borderless1942]: [{DateTime.Now:MM/dd/yyyy - hh:mm:ss tt}] [BF1942 Process Has Started]: ({process.Id})");
+Console.WriteLine($"[Borderless1942]: [{DateTime.Now:yyyy-MM-dd - hh:mm:ss tt}] [BF1942 Process Has Started] [{process.Id}]");
 
 // Wait for initial window handle
 var window = await process.WaitForMainWindowAsync();
@@ -25,47 +22,41 @@ window.RemoveBorders();
 while (keepAlive)
 {
 MainLoop:
+	UpdateWindowPosition(window);
+	await Task.Delay(100);
 
-    UpdateWindowPosition(window);
-    await Task.Delay(100);
-
-    if (process.HasExited)
-    {
-        var oldProcessId = process.Id;
-        var retryCount = 0;
-
-        while (retryCount < 2)
-        {
-            var matchingProcesses = Process.GetProcessesByName("BF1942");
-            if (matchingProcesses.Length == 1)
-            {
-                process = matchingProcesses[0];
-
-                Console.WriteLine($"[Borderless1942]: [{DateTime.Now:MM/dd/yyyy - hh:mm:ss tt}] [BF1942 Process Has Changed] [Map Change Detected] ({oldProcessId} -> {process.Id})");
-
-                if (oldProcessId == process.Id)
-                {
-                    Console.WriteLine($"[Borderless1942]: [{DateTime.Now:MM/dd/yyyy - hh:mm:ss tt}] [BF1942 Process Has Been Closed] ({oldProcessId} -> {process.Id})");
-                    keepAlive = false;
-                    break;
-                }
-
-                window = await process.WaitForMainWindowAsync();
-                window.RemoveBorders();
-
-                goto MainLoop;
-            }
-            retryCount++;
-            await Task.Delay(TimeSpan.FromSeconds(1));
-        }
-    }
+	if (process.HasExited)
+	{
+		var oldProcessId = process.Id;
+		var retryCount = 0;
+		while (retryCount < 3)
+		{
+			var matchingProcesses = Process.GetProcessesByName("BF1942");
+			if (matchingProcesses.Length == 1)
+			{
+				process = matchingProcesses[0];
+				if (oldProcessId == process.Id)
+				{
+					Console.WriteLine($"[Borderless1942]: [{DateTime.Now:yyyy-MM-dd - hh:mm:ss tt}] [BF1942 Process Has Exited] [Closing Program] [{oldProcessId} -> {process.Id}]");
+					keepAlive = false;
+					Environment.Exit(0);
+				}
+				Console.WriteLine($"[Borderless1942]: [{DateTime.Now:yyyy-MM-dd - hh:mm:ss tt}] [BF1942 Process Has Changed] [{oldProcessId} -> {process.Id}]");
+				window = await process.WaitForMainWindowAsync();
+				window.RemoveBorders();
+				goto MainLoop;
+			}
+			retryCount++;
+			await Task.Delay(TimeSpan.FromSeconds(1));
+		}
+	}
 }
 
 static void UpdateWindowPosition(Window window)
 {
-    var monitorBounds = window.GetCurrentMonitor().GetBounds();
-    var windowBounds = window.GetBounds();
-    var x = monitorBounds.Width / 2 - windowBounds.Width / 2;
-    var y = monitorBounds.Height / 2 - windowBounds.Height / 2;
-    window.SetPosition(x, y);
+	var monitorBounds = window.GetCurrentMonitor().GetBounds();
+	var windowBounds = window.GetBounds();
+	var x = monitorBounds.Width / 2 - windowBounds.Width / 2;
+	var y = monitorBounds.Height / 2 - windowBounds.Height / 2;
+	window.SetPosition(x, y);
 }
